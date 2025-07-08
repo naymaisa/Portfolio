73bccc18657f8e1de646292370261d9e1bee2499
Nayara <nayaramaisa.stn@gmail.com>
2025-07-08 12:14:49 -0300

    portifolio subindo pra vercel 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { AboutComponent } from "./components/about/about.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutComponent, RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'sobre', component: AboutComponent },
+  { path: 'projetos', component: SkillsComponent },
+  { path: 'contato', component: ContactComponent }
+];
